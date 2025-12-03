462aea001970d1818db597efc12c4ad7568d2da9
Elisabeth Garner <garner.395@buckeyemail.osu.edu>
2025-12-03 12:11:18 -0500

    Progress on star index script, finished and just need to look over

---
+++ star/run_star_index.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+#SBATCH --account=PAS2880
+#SBATCH --mail-type=END,FAIL
+#SBATCH --output=slurm-downloads-%j.out
+#SBATCH --time=12:00:00
+#SBATCH --cpus-per-task=8
+
+set -euo pipefail
+
+fasta=$1
+gtf=$2
+outdir=$3
+
+STAR=oras://community.wave.seqera.io/library/star:2.7.11b--84fcc19fdfab53a4
+
+# Initial logging
+
+echo "Starting script run_star_index.sh"
+date
+
+# Making the output dir
+
+mkdir -p "$outdir"
+
+# Loading & Running STAR for the index
+
+apptainer exec "$STAR" STAR \
+    --runMode genomeGenerate \
+     --genomeFastaFiles "$fasta" \
+     --genomeDir "$outdir" \
+     --sjdbGTFfile "$gtf" \
+     --runthreadN 16 \
+     --sjdbOverhang 79 \
+     --genomeSAindexNbases 13
+
+# Final logging
+echo 
+echo "Finished script run_trimgalore.sh"
+apptainer exec "$TRIMGALORE" trim_galore --version
+date
